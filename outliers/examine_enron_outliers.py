b7f58796e96e5a93909e9522668cf03dbb4901a3
Atilla Ozgur <ati.ozgur@gmail.com>
2015-12-09 12:18:50 +0200

    small fix

--- outliers/examine_enron_outliers.py
+++ outliers/examine_enron_outliers.py
@@ -1,3 +1,6 @@
 import pandas as pd
 df = pd.DataFrame.from_dict(data_dict,orient='index')
 df[['bonus', 'salary']] = df[['bonus', 'salary']].astype(float)
+
+df.sort(columns="salary",ascending=False).head()["salary"]
+df.sort(columns="bonus",ascending=False).head()["bonus"]
